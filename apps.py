0a1481a36224845c4e51bcbdb601d38f19028632
akashdas2110 <112683602+akashdas2110@users.noreply.github.com>
2022-12-20 11:47:07 +0530

    Update apps.py

--- apps.py
+++ apps.py
@@ -1,11 +1,9 @@
 import streamlit as st
 import pickle
 import string
-
-import nltk
-nltk.download("punkt")
-nltk.download('stopwords')
+nltk.download(‘stopwords’)
 from nltk.corpus import stopwords
+import nltk
 from nltk.stem.porter import PorterStemmer
 
 ps = PorterStemmer()
